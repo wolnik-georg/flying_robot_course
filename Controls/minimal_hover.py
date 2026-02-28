cf782242e1b16d1171b9f5f744d792b75b206664
Georg Wolnik <wolnik.georg0317@gmail.com>
2026-02-28 16:08:07 +0100

    simple movements work.

--- Controls/minimal_hover.py
+++ Controls/minimal_hover.py
@@ -5,17 +5,22 @@ from threading import Event
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils import uri_helper
 
+
 URI = uri_helper.uri_from_env(default="radio://0/80/2M/E7E7E7E7E7")
 
-DEFAULT_HEIGHT = 0.3
+DEFAULT_HEIGHT = 0.5
+BOX_LIMIT = 0.3
 
 deck_attached_event = Event()
 
 logging.basicConfig(level=logging.ERROR)
+
+position_estimate = [0, 0]
 
 
 def take_off_simple(scf):
@@ -37,12 +42,45 @@ def param_deck_flow(_, value_str):
 def move_linear_simple(scf):
     with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
         time.sleep(1)
-        mc.forward(0.5)
-        time.sleep(1)
-        mc.turn_left(180)
-        time.sleep(1)
-        mc.forward(0.5)
-        time.sleep(1)
+        mc.forward(0.3)
+        mc.turn_left(90)
+        mc.forward(0.3)
+        mc.turn_left(90)
+        mc.forward(0.3)
+        mc.turn_left(90)
+
+
+def log_pos_callback(timestamp, data, logconf):
+    print(data)
+    global position_estimate
+    position_estimate[0] = data["stateEstimate.x"]
+    position_estimate[1] = data["stateEstimate.y"]
+
+
+def move_box_limit(scf):
+    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
+        body_x_cmd = 0.2
+        body_y_cmd = 0.1
+        max_vel = 0.2
+
+        while 1:
+            # if position_estimate[0] > BOX_LIMIT:
+            #    mc.start_back()
+            # elif position_estimate[0] < -BOX_LIMIT:
+            #    mc.start_forward()
+
+            if position_estimate[0] > BOX_LIMIT:
+                body_x_cmd = -max_vel
+            elif position_estimate[0] < -BOX_LIMIT:
+                body_x_cmd = max_vel
+            if position_estimate[1] > BOX_LIMIT:
+                body_y_cmd = -max_vel
+            elif position_estimate[1] < -BOX_LIMIT:
+                body_y_cmd = max_vel
+
+            mc.start_linear_motion(body_x_cmd, body_y_cmd, 0)
+
+            time.sleep(0.1)
 
 
 if __name__ == "__main__":
@@ -55,6 +93,12 @@ if __name__ == "__main__":
         )
         time.sleep(1)
 
+        logconf = LogConfig(name="Position", period_in_ms=10)
+        logconf.add_variable("stateEstimate.x", "float")
+        logconf.add_variable("stateEstimate.y", "float")
+        scf.cf.log.add_config(logconf)
+        logconf.data_received_cb.add_callback(log_pos_callback)
+
         if not deck_attached_event.wait(timeout=5):
             print("No flow deck detected!")
             sys.exit(1)
@@ -63,5 +107,6 @@ if __name__ == "__main__":
         scf.cf.platform.send_arming_request(True)
         time.sleep(1.0)
 
-        # take_off_simple(scf)
+        logconf.start()
         move_linear_simple(scf)
+        logconf.stop()
