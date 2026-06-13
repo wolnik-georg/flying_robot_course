3824456bc8689191a0bddba6ddc790ba36872490
Georg Wolnik <wolnik.georg1703@gmail.com>
2026-06-13 13:57:32 +0200

    new logs

--- Controls/install/setup.zsh
+++ Controls/install/setup.zsh
@@ -24,6 +24,9 @@ _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
 COLCON_CURRENT_PREFIX="/home/flyingrobots/georg/ros2_ws/install"
 _colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
+# setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
+COLCON_CURRENT_PREFIX="/home/flyingrobots/georg/crazyflie-firmware/install"
+_colcon_prefix_chain_zsh_source_script "$COLCON_CURRENT_PREFIX/local_setup.zsh"
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
